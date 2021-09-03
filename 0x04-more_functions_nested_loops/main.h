edecfe187a506d3a1a216a3fd5428bd551bff8d0
JerrySimba <salibursalibur3@gmail.com>
2021-09-03 10:06:52 -0700

    included main.h, file prints uppercase

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,1 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Author: Simba Jeremiah
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x03-more_functions_nested_loops directory.
+ */
+
+int _putchar(char c);
 int _isupper(int c);
+
+#endif
